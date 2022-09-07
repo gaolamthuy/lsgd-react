2600ea9dad0eb415e16d396b8e2b12070399a338
Lam Ho <hophamlam92@yahoo.com>
2022-09-07 18:01:34 +0700

    test

--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -1,35 +1,33 @@
 const { schedule } = require("@netlify/functions");
 
 const handler = async function () {
-  const sendMessageDiscord = () => {
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
-    myHeaders.append(
-      "Cookie",
-      "__dcfduid=0f7577621c7811edbf7cdeffafb5e155; __sdcfduid=0f7577621c7811edbf7cdeffafb5e155fa163fdf4b26c835f5b8fa76777cef0c6ea7b05755fff7e0a324e03446dc7f86"
-    );
+  var axios = require("axios");
+  var FormData = require("form-data");
+  var data = new FormData();
+  data.append("content", "test");
 
-    var formdata = new FormData();
-    formdata.append("content", "test");
-
-    var requestOptions = {
-      method: "POST",
-      headers: myHeaders,
-      body: formdata,
-      redirect: "follow",
-    };
-
-    fetch(
-      "https://discord.com/api/webhooks/1010911250743570502/Ixj4EiWE8-DkNAqulQxNHsIRiB7X5CInCVDnEH_Uzmkh2Hu_CjeeBSrBFDtAmCcQSXnK",
-      requestOptions
-    )
-      .then((response) => response.text())
-      .then((result) => console.log(result))
-      .catch((error) => console.log("error", error));
+  var config = {
+    method: "post",
+    url: "https://discord.com/api/webhooks/1010911250743570502/Ixj4EiWE8-DkNAqulQxNHsIRiB7X5CInCVDnEH_Uzmkh2Hu_CjeeBSrBFDtAmCcQSXnK",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Cookie:
+        "__dcfduid=0f7577621c7811edbf7cdeffafb5e155; __sdcfduid=0f7577621c7811edbf7cdeffafb5e155fa163fdf4b26c835f5b8fa76777cef0c6ea7b05755fff7e0a324e03446dc7f86",
+      ...data.getHeaders(),
+    },
+    data: data,
   };
+
+  axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
   console.log("Received event:");
 
-  sendMessageDiscord();
   return {
     statusCode: 200,
   };
