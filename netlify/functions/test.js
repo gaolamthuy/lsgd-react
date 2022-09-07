3cc21bd59cfe41bbdf8278378b9ffd6748099f11
Lam Ho <hophamlam92@yahoo.com>
2022-09-07 17:42:27 +0700

    test

--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -12,4 +12,4 @@ const handler = async function () {
 
 //Every 2 minutes, at 12:00 AM through 11:00 AM and 11:59 PM (Times shown in UTC)
 // module.exports.handler = schedule("*/2 0-11,23 * * *", handler);
-module.exports.handler = schedule("*/30 * * * *", handler);
+module.exports.handler = schedule("* * * * *", handler);
