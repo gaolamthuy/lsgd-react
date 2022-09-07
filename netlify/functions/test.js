53c0605505fa8ada2c08f616cc24fb7e3eeb1c9b
Lam Ho <hophamlam92@yahoo.com>
2022-09-07 20:27:50 +0700

    test

--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -2,9 +2,9 @@ const { schedule } = require("@netlify/functions");
 
 const fetch = require("node-fetch");
 
-const API_ENDPOINT = "https://api.randomuser.me";
+const API_ENDPOINT = "https://cat-fact.herokuapp.com/facts";
 
-const handler = async (event, context) => {
+exports.handler = async (event, context) => {
   let response;
   try {
     response = await fetch(API_ENDPOINT);
@@ -28,4 +28,4 @@ const handler = async (event, context) => {
 
 //Every 2 minutes, at 12:00 AM through 11:00 AM and 11:59 PM (Times shown in UTC)
 // module.exports.handler = schedule("*/2 0-11,23 * * *", handler);
-module.exports.handler = schedule("*/2 * * * *", handler);
+// module.exports.handler = schedule("*/2 * * * *", handler);
