9b4c8ed154c0ed4abac959ac23d9c45cdb7a66a3
Lam Ho <hophamlam92@yahoo.com>
2023-02-28 15:17:26 +0700

    update

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@ if [ -d "./lsgd-react" ]
 then
     sudo git -C ./lsgd-react pull 
     
-    sudo docker-compose -f ./lsgd-react/docker-compose.prod.yml build
-    sudo docker-compose -f ./lsgd-react/docker-compose.prod.yml up
+    sudo docker-compose -f ~/lsgd-react/docker-compose.prod.yml build
+    sudo docker-compose -f ~/lsgd-react/docker-compose.prod.yml up
 else
     sudo git clone https://github.com/hophamlam/lsgd-react.git
     
